b52aa2a21f0909a374351b4b8d86bbd1ed9d93c0
Pont-SimoSiren <sirensimo7@gmail.com>
2024-04-08 13:05:20 +0300

    laskuri valmis

--- src/Laskuri.jsx
+++ src/Laskuri.jsx
@@ -6,12 +6,18 @@ function Laskuri() {
     // Komponentin state on nimeltään luku, joka alustetaan alussa nollaksi
     // setLuku funktiolla voidaan asettaa luku state
     const [luku, setLuku] = useState(0)
+    const [show, setShow] = useState(false)
 
   return (
     <>
-        <h2>{luku}</h2>
+        {!show && <button onClick={() => setShow(true)}>Näytä laskuri</button>}
+        {show && <button onClick={() => setShow(false)}>Piilota laskuri</button>}
 
-        <button onClick={() => setLuku(luku + 1)}>+</button>
+        {show && <h2>{luku}</h2>}
+        {show && <button onClick={() => setLuku(luku + 1)}>PLUS</button>}
+        {show && <button onClick={() => setLuku(luku + 999999999)}>UUS ENKKA CHEAT</button>}
+        {show && <button onClick={() => setLuku(luku - 1)}>MIINUS</button>}
+        {show && <button onClick={() => setLuku(0)}>RESET</button>}
     </>
   )
 }
